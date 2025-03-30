d1d44c2c5e7dcdf8273fe182f7f761fd2eb1b75d
Janaka Chamith <janakachamith88@gmail.com>
2025-03-30 17:47:19 +0530

    final updated

--- app/api/analytics/lastweek/route.ts
+++ app/api/analytics/lastweek/route.ts
@@ -7,7 +7,7 @@ import { startOfDay, subDays } from "date-fns";
 import geoDistance from "geo-distance";
 import { RADIUS } from "@/constants";
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest) {
   const user = await isLoggedIn(req);
   if (!user) {
     return NextResponse.json(
